755a44a450b7baef2e922a04b15dc34c2f638f51
Mahmoud Almahroum <mahmoud.al808@gmail.com>
2020-03-28 18:46:35 +0000

    Added google sign in button to sign in screen

--- app/src/main/java/com/example/remed/SignInActivity.java
+++ app/src/main/java/com/example/remed/SignInActivity.java
@@ -9,21 +9,13 @@ import android.widget.Button;
 
 public class SignInActivity extends AppCompatActivity {
 
-    Button sign_in_button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
 
-        sign_in_button = findViewById(R.id.sign_in_button);
-        sign_in_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(SignInActivity.this, HomePage.class);
-                startActivity(intent);
-            }
-        });
 
     }
 }
