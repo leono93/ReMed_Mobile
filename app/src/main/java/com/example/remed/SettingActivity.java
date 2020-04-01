5238c7e782ff85e72a9b7f3abfab0d4e14d58016
Mahmoud Almahroum <mahmoud.al808@gmail.com>
2020-04-01 10:53:46 +0100

    Added google log out code

--- app/src/main/java/com/example/remed/SettingActivity.java
+++ app/src/main/java/com/example/remed/SettingActivity.java
@@ -2,13 +2,60 @@ package com.example.remed;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SettingActivity extends AppCompatActivity {
+
+    private GoogleSignInClient googleSignInClient;
+    private Context context;
+    private Button logout_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+        init();
+        setUpWidgets();
     }
+
+    private void init(){
+        logout_btn = findViewById(R.id.logout_btn);
+        context = SettingActivity.this;
+    }
+
+    private void setUpWidgets(){
+        logout_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                logout();
+            }
+        });
+    }
+
+    public void logout() {
+        setUpGooogleSignin();
+        FirebaseAuth.getInstance().signOut();
+        googleSignInClient.signOut();
+        Intent intent = new Intent(this.context, SignInActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        this.context.startActivity(intent);
+    }
+
+    private void setUpGooogleSignin() {
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(context.getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        googleSignInClient = GoogleSignIn.getClient(this.context, gso);
+    }
+
 }
