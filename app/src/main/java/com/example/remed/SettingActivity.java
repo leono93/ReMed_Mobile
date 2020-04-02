352bb5fbb7235f45a09d1aa3b6c900f884744820
윤 다은 / DaeunYoon <34188262+DaeunYoon@users.noreply.github.com>
2020-04-02 20:07:50 +0100

    Add files via upload

--- app/src/main/java/com/example/remed/SettingActivity.java
+++ app/src/main/java/com/example/remed/SettingActivity.java
@@ -1,12 +1,16 @@
 package com.example.remed;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.ScaleDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -18,6 +22,7 @@ public class SettingActivity extends AppCompatActivity {
     private GoogleSignInClient googleSignInClient;
     private Context context;
     private Button logout_btn;
+    private ImageButton back_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +34,7 @@ public class SettingActivity extends AppCompatActivity {
 
     private void init(){
         logout_btn = findViewById(R.id.logout_btn);
+        back_btn = findViewById(R.id.backButton);
         context = SettingActivity.this;
     }
 
@@ -37,6 +43,13 @@ public class SettingActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 logout();
+            }
+        });
+
+        back_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
             }
         });
     }
