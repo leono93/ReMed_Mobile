917e24a09feb38898f3ac36cdd2b5ba1c9a145a8
Mahmoud Almahroum <mahmoud.al808@gmail.com>
2020-04-19 13:33:02 +0100

    Added intent to image on welcome screen

--- app/src/main/java/com/example/remed/MainActivity.java
+++ app/src/main/java/com/example/remed/MainActivity.java
@@ -5,16 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView welcome_title;
+    ImageView welcome_image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        welcome_image = findViewById(R.id.welcome_image);
+
+        welcome_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SignInActivity.class);
+                startActivity(intent);
+            }
+        });
 
         welcome_title = findViewById(R.id.welcome_title);
         welcome_title.setOnClickListener(new View.OnClickListener() {
